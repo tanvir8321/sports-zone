844d93cfcd20009e57f40dc7143eaa9360e447d9
tansovir <tanvir8321@gmail.com>
2021-06-24 10:00:43 +0600

    initial

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className="headerContainer">
+            <h1>Sports Zone</h1>
+        </div>
+    );
+};
+
+export default Header;
